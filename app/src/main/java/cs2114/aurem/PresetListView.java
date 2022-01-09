56165772a05b690866cfa3ad0fa0c2f6d975a464
Michelangelo De Pascale <michelangelodepascale02@gmail.com>
2022-01-09 19:25:48 +0100

    UI changes. it-IT localization.
    Added Italian language. Removed hard-coded text strings and replaced with appropriate attributes in strings.xml files. Modified the graphical interface, trying to imitate (as much as possible) the design of the most modern Android versions. Added a button for credits. Updated image assets. Custom theme for scrollbars.

---
+++ app/src/main/java/cs2114/aurem/PresetListView.java
@@ -0,0 +1,50 @@
+package cs2114.aurem;
+
+import android.content.Intent;
+import android.view.View;
+import android.view.Window;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.os.Bundle;
+import android.app.ListActivity;
+
+/**
+ * // -------------------------------------------------------------------------
+/**
+ *  This is the ListView containing all the Equalizer presets
+ *  that the user can select to load.
+ *
+ *  @author Joseph O'Connor (jto2e);
+ *  @author Laura Avakian (lavakian);
+ *  @author Barbara Brown (brownba1);
+ *  @version 2012.04.18
+ */
+public class PresetListView extends ListActivity
+{
+    private String[] names;
+
+    /**
+     * This is called when the Activity is stared.
+     * @param savedInstanceState Bundle the saved instance state.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        names = getIntent().getStringArrayExtra("names");
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item
+            , names));
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id)
+    {
+        super.onListItemClick(l, v, position, id);
+        Intent intent = new Intent();
+        intent.putExtra("index", position);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
+
